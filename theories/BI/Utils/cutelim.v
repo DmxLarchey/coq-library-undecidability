c9c9ed0e451baefbeafd30fbc5beddc0b8fe1021
DmxLarchey <larchey@gmail.com>
2026-09-07 15:02:24 +0200

    cut elim proof for fragments of BI

---
+++ theories/BI/Utils/cutelim.v
@@ -0,0 +1,15 @@
+(**************************************************************)
+(*   Copyright Dominique Larchey-Wendling [*]                 *)
+(*                                                            *)
+(*                             [*] Affiliation LORIA -- CNRS  *)
+(**************************************************************)
+(*      This file is distributed under the terms of the       *)
+(*        Mozilla Public License Version 2.0, MPL-2.0         *)
+(**************************************************************)
+
+From Stdlib Require Import List Utf8.
+
+From Undecidability.BI
+  Require Import BI utils.
+
+Import BI_notations ListNotations.
